e36707af0fcf88c28832f0d84907c175c6a48395
rockerBOO <rockerboo@gmail.com>
2020-09-04 03:27:35 -0400

    Initial

---
+++ lib/bluur.ex
@@ -0,0 +1,18 @@
+defmodule Bluur do
+  @moduledoc """
+  Documentation for `Bluur`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bluur.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bluur_test.exs
@@ -0,0 +1,8 @@
+defmodule BluurTest do
+  use ExUnit.Case
+  doctest Bluur
+
+  test "greets the world" do
+    assert Bluur.hello() == :world
+  end
+end
